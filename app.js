ad71c5cb706d59d6c581216d81433689de3d3256
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2017-02-16 22:41:43 -0800

    add a route and nodemon

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+app.get('/', (req,res) => {
+  res.send('Invalid Endpoint');
+});
+
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
