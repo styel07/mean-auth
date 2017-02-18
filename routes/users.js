9994d18d5dc5364c3eb9423d5ff0da619f21f38d
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2017-02-17 20:59:12 -0800

    created config public and routes folders and files

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+// Registrer
+router.get('/register', (req, res, next) => {
+  res.send('register');
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  res.send('Authenticate');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+  res.send('Profile');
+});
+// Validate
+router.get('/validate', (req, res, next) => {
+  res.send('Validate');
+});
+
+module.exports = router;
